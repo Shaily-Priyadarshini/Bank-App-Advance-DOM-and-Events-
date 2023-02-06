7953fb0d86b0ca5503d7a1459de0671594d29b4e
Shaily Priyadarshini <priyadarshinishaily@gmail.com>
2023-02-06 14:34:01 +0530

    Implementing features with Advance DOM and Events

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+const btnOpenModal=document.querySelector('.nav__link--btn')
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+btnOpenModal.addEventListener('click',function(e){
+  e.preventDefault();
+});
+btnsOpenModal.forEach(btn=>btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+//smooth scrolling
